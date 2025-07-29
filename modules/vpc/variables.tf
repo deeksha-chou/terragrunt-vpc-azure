8ebb6e21c50764c65e83ffba99c29421e91f745d
Deeksha <126566749+deeksha-chou@users.noreply.github.com>
2025-07-29 23:06:20 +0200

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,19 +1,51 @@
-variable "project_name" {}
-variable "vpc_cidr" {}
-variable "public_subnet_az1_cidr" {}
-variable "public_subnet_az2_cidr" {}
-variable "private_app_subnet_az1_cidr" {}
-variable "private_app_subnet_az2_cidr" {}
-variable "private_data_subnet_az1_cidr" {}
-variable "private_data_subnet_az2_cidr" {}
+# Tip: keep variable descriptions for clarity & docs.
 
-
-variable "availability_zones" {
-  type    = list(string)
-  default = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+variable "project_name" {
+  type        = string
+  description = "Project/application name for tagging."
 }
 
 variable "environment" {
   type        = string
-  description = "Environment name i.e dev or prod"
+  description = "Environment name for tagging (e.g., dev, prod)."
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR for the VPC (e.g., 10.0.0.0/16)."
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of 2 availability zones to place subnets."
+}
+
+variable "public_subnet_az1_cidr" {
+  type        = string
+  description = "CIDR for public subnet in AZ1."
+}
+
+variable "public_subnet_az2_cidr" {
+  type        = string
+  description = "CIDR for public subnet in AZ2."
+}
+
+variable "private_app_subnet_az1_cidr" {
+  type        = string
+  description = "CIDR for private app subnet in AZ1."
+}
+
+variable "private_app_subnet_az2_cidr" {
+  type        = string
+  description = "CIDR for private app subnet in AZ2."
+}
+
+variable "private_data_subnet_az1_cidr" {
+  type        = string
+  description = "CIDR for private data subnet in AZ1."
+}
+
+variable "private_data_subnet_az2_cidr" {
+  type        = string
+  description = "CIDR for private data subnet in AZ2."
 }
