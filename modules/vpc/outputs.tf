e1ef483fa7ab785dfa8e01b52e65df855128ff69
Deeksha <126566749+deeksha-chou@users.noreply.github.com>
2025-07-29 23:07:00 +0200

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,36 +1,29 @@
-
-output "project_name" {
-  value = var.project_name
-}
-
 output "vpc_id" {
-  value = aws_vpc.vpc.id
-
-}
-output "public_subnet_az1_id" {
-  value = aws_subnet.public_subnet_az1.id
+  value       = aws_vpc.vpc.id
+  description = "VPC ID."
 }
 
-output "public_subnet_az2_id" {
-  value = aws_subnet.public_subnet_az2.id
+output "igw_id" {
+  value       = aws_internet_gateway.internet_gateway.id
+  description = "Internet Gateway ID."
 }
 
-output "private_app_subnet_az1_id" {
-  value = aws_subnet.private_app_subnet_az1.id
+output "public_subnet_ids" {
+  value       = [aws_subnet.public_subnet_az1.id, aws_subnet.public_subnet_az2.id]
+  description = "Public subnet IDs."
 }
 
-output "private_app_subnet_az2_id" {
-  value = aws_subnet.private_app_subnet_az2.id
+output "private_app_subnet_ids" {
+  value       = [aws_subnet.private_app_subnet_az1.id, aws_subnet.private_app_subnet_az2.id]
+  description = "Private application subnet IDs."
 }
 
-output "private_data_subnet_az1_id" {
-  value = aws_subnet.private_data_subnet_az1.id
+output "private_data_subnet_ids" {
+  value       = [aws_subnet.private_data_subnet_az1.id, aws_subnet.private_data_subnet_az2.id]
+  description = "Private data subnet IDs."
 }
 
-output "private_data_subnet_az2_id" {
-  value = aws_subnet.private_data_subnet_az2.id
-}
-
-output "internet_gateway" {
-  value = aws_internet_gateway.internet_gateway
+output "public_route_table_id" {
+  value       = aws_route_table.public_route_table.id
+  description = "Public route table ID."
 }
